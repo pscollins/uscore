c1e43d5ce7767e42dc5c00f5f04cf22ca8afd67e
Patrick <pscollins@uchicago.edu>
2013-12-31 05:50:27 -0500

    template mostly written

--- app.py
+++ app.py
@@ -2,10 +2,20 @@ import flask
 
 app = flask.Flask(__name__)
 
+@app.route('/about')
+def about()
+    return 'foo'
 
 @app.route('/')
-def main():
-    return flask.render_template('index.html', title = 'FOO')
+@app.route('/main/<key>')
+def display_table(key='in comments count unique'):
+    title = 'FOO'
+    column_names = ['a', 'b', 'c']
+    rows = [[1, 2, 3] for x in range(0, 10)]
+    return flask.render_template('index.html',
+                                 title=title,
+                                 column_names=column_names,
+                                 rows=rows)
 
 
 if __name__ == '__main__':
