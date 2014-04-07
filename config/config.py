3d09f7ddb0ffd7492f82eee30965f2cc5762102d
CapitalSigma <pscollins@uchicago.edu>
2014-04-07 00:36:40 -0500

    database refactor on the way

--- config/config.py
+++ config/config.py
@@ -1,4 +1,7 @@
+import os
 
-
-
-SQLALCHEMY_DB_URI = "../sqlite.db"
+BASE = ".."
+DATA = "data"
+_RELATIVE_SQLALCHEMY_DB_URI = os.path.join(DATA, "sqlite.db")
+SQLALCHEMY_DB_URI = "sqlite:///{}".format(
+    os.path.join(BASE, _RELATIVE_SQLALCHEMY_DB_URI))
